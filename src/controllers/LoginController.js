ff59a76ff98c7f7f2ffabe4a5bb8459c6c31638a
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-10 18:09:04 -0300

    feat: add login controller

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -8,10 +8,25 @@ const login = (request, response) => {
   response.send(request.body);
 };
 
-const register = (request, response) => {
-  const login = new Login(request.body);
-  login.register();
-  response.send(login.user);
+const register = async (request, response) => {
+  try {
+    const login = new Login(request.body);
+    await login.register();
+    if (login.errors.length > 0) {
+      request.flash("errors", login.errors);
+      request.session.save(function () {
+        return response.redirect(303, request.get("Referer") || "/login");
+      });
+      return;
+    }
+    request.flash("success", "Cadastro realizado com sucesso!");
+    request.session.save(function () {
+      return response.redirect(303, request.get("Referer") || "/login");
+    });
+  } catch (error) {
+    console.error(error);
+    return response.send("404");
+  }
 };
 
 export { index, login, register };
