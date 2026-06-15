229b19d4467a9e48076c451f70b9465f3f37422d
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-15 14:52:06 -0300

    feat: add register

--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -1,5 +1,26 @@
+import Contact from "../models/ContactModel.js";
+
 const index = (request, response) => {
   return response.render("contact");
 };
 
-export { index };
+const register = async (request, response) => {
+  try {
+    const contact = new Contact(request.body);
+    await contact.register(request.session.user);
+    if (contact.errors.length > 0) {
+      request.flash("errors", contact.errors);
+      return response.redirect("/contato");
+    }
+    request.flash("success", "Contato cadastrado com sucesso!");
+    return response.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const list = async (error, request, response) => {};
+
+const edit = async (error, request, response) => {};
+
+export { index, register, list, edit };
